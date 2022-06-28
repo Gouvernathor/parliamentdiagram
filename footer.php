3c55e976cc9271e72a5f99692ab9ef4b567fa602
Asriel Senna <aanonyme.personne@hotmail.fr>
2022-06-28 19:56:00 +0200

    credit myself

--- footer.php
+++ footer.php
@@ -3,8 +3,9 @@
     <p>Built by
 <a href="https://en.wikipedia.org/wiki/User:Slashme" target="_blank">David Richfield</a>,
 <a href="https://github.com/Rade-Mathis" target="_blank">Mathis Rade</a>,
-<a href="https://en.wikipedia.org/wiki/User:Ranjithsiji" target="_blank">Ranjith Siji</a> and
-<a href="https://en.wikipedia.org/wiki/User:Ambadyanands" target="_blank">Ambady Anand S</a>.
+<a href="https://en.wikipedia.org/wiki/User:Ranjithsiji" target="_blank">Ranjith Siji</a>
+<a href="https://en.wikipedia.org/wiki/User:Ambadyanands" target="_blank">Ambady Anand S</a> and
+<a href="https://github.com/Gouvernathor" target="_blank">Gouvernathor</a>.
 Code licensed under <span><a href="https://raw.githubusercontent.com/Gouvernathor/parliamentdiagram/master/LICENSE.md" target="_blank">GPL v2</a></span>.</p>
     <ul class="base-footer-links muted">
       <li><a href="https://github.com/Gouvernathor/parliamentdiagram">Source repository</a></li>
